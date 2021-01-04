5814ef7a6bffe3d0753572a0caedd7e23f7ba021
Liliya Kaplan <gafifettey@gmail.com>
2021-01-03 23:19:41 -0500

    some changes

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -11,44 +11,68 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 
 public class Driver {
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     private Driver() {}
 
     public static WebDriver getDriver() {
-        if (driver == null) {
-            String browser = ConfigurationReader.getProperty("browser");
-            switch (browser) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                case "remote-chrome":
-                    try {
-                        //ChromeOptions chromeOptions = new ChromeOptions();
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("chrome");
-                        URL gridUrl = new URL("http://100.27.21.229:4444/wd/hub");
-                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
-                default:
-                    throw new RuntimeException("No such a browser yet!");
+        if (driverPool.get() == null) {
+
+            synchronized (Driver.class) {
+
+                String browser = ConfigurationReader.getProperty("browser");
+
+                if (System.getProperty("browser") != null) {
+//                then change browser type
+//                regardless on value configuration.properties
+                    System.out.println("Browser type was changed to: " + System.getProperty("browser"));
+                    browser = System.getProperty("browser");
+                }
+
+
+                switch (browser) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        break;
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        driverPool.set(new FirefoxDriver());
+                        break;
+                    case "remote-chrome":
+                        try {
+                            //ChromeOptions chromeOptions = new ChromeOptions();
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("chrome");
+                            URL gridUrl = new URL("http://34.204.1.74:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridUrl, desiredCapabilities));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "remote-firefox":
+                        try {
+                            //ChromeOptions chromeOptions = new ChromeOptions();
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("firefox");
+                            URL gridUrl = new URL("http://34.204.1.74:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridUrl, desiredCapabilities));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("No such a browser yet!");
+                }
             }
         }
-        return driver;
+        return driverPool.get();
     }
 
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 }
